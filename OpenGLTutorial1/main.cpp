ee7c2bbceac9c425e4d0fa372a2a77e9c2ece729
Michael Street <mstreet@qti.qualcomm.com>
2015-01-24 16:24:41 -0800

    Added shader handling

--- OpenGLTutorial1/main.cpp
+++ OpenGLTutorial1/main.cpp
@@ -1,17 +1,21 @@
 #include <iostream>
 
 #include "display.h"
+#include "shader.h"
 
 int main(int argc, char** argv)
 {
     Display display(800,600, "Hello World");
 
+    Shader shader("./res/basicShader");
+
     while (!display.IsClosed())
     {
         display.Clear(0.0f, 0.15f, 0.35f, 1.0f);
+        shader.Bind();
+
         display.Update();
     }
-
 
     return 0;
 }
