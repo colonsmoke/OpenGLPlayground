cedfaaa6532a5f03f8f73e842a165537998be298
Michael Street <mstreet@qti.qualcomm.com>
2015-01-25 15:21:10 -0800

    Added lighting and mesh object loading

--- OpenGLTutorial1/main.cpp
+++ OpenGLTutorial1/main.cpp
@@ -13,13 +13,9 @@
 int main(int argc, char** argv)
 {
     Display display(WIDTH, HEIGHT, "OpenGL Playground");
-
     Shader shader("./res/basicShader");
-
     Texture texture("./res/brick.jfif");
-
-    Camera camera(glm::vec3(0, 0, -3), 70.0, (float)WIDTH / (float)HEIGHT, 0.01f, 1000.0f);
-
+    Camera camera(glm::vec3(0, 0, -4), 70.0, (float)WIDTH / (float)HEIGHT, 0.01f, 1000.0f);
     Transform transform;
 
     float counter = 0.0f;
@@ -28,7 +24,12 @@ int main(int argc, char** argv)
                             Vertex(glm::vec3(0, 0.5, 0), glm::vec2(0.5,1.0)),
                             Vertex(glm::vec3(0.5, -0.5, 0), glm::vec2(1.0, 0.0)) };
 
-    Mesh mesh(vertices, sizeof(vertices) / sizeof(vertices[0]));
+    unsigned int indices[] = { 0, 1, 2 };
+
+    Mesh mesh(vertices, sizeof(vertices) / sizeof(vertices[0]),
+                indices, sizeof(indices) / sizeof(indices[0]) );
+
+    Mesh mesh2("./res/monkey3.obj");
 
     while (!display.IsClosed())
     {
@@ -40,14 +41,13 @@ int main(int argc, char** argv)
         transform.GetPos().x = sinCounter;
         transform.GetPos().z = cosCounter;
         transform.GetRot().z = counter;
-        transform.GetRot().y = counter;
+        transform.GetRot().y = counter + 180.0;
         transform.GetRot().x = counter;
-        // transform.SetScale(glm::vec3(cosCounter, cosCounter, cosCounter));
 
         shader.Bind();
         shader.Update(transform, camera);
         texture.Bind(0);
-        mesh.Draw();
+        mesh2.Draw();
 
         GLenum myError = glGetError();
 
