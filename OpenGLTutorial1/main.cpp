eb0227af84117168fff5d11c8535d642149bcda4
Michael Street <colonsmoke@mac.com>
2015-01-25 19:31:36 -0800

    Added Enterprise experiment

--- OpenGLTutorial1/main.cpp
+++ OpenGLTutorial1/main.cpp
@@ -8,14 +8,16 @@
 #include "transform.h"
 #include "camera.h"
 
-#define WIDTH 1280
-#define HEIGHT 1024
+#define WIDTH 1024
+#define HEIGHT 768  
+
 int main(int argc, char** argv)
 {
     Display display(WIDTH, HEIGHT, "OpenGL Playground");
     Shader shader("./res/basicShader");
-    Texture texture("./res/whiteGloss.jpg");
-    Camera camera(glm::vec3(0, 0, -7), 70.0, (float)WIDTH / (float)HEIGHT, 0.01f, 1000.0f);
+    Texture texture("./res/enterprise.png");
+    Texture texture2("./res/silver-metal.jpg");
+    Camera camera(glm::vec3(0, 0, -10), 70.0, (float)WIDTH / (float)HEIGHT, 0.01f, 1000.0f);
     Transform transform;
 
     float counter = 0.0f;
@@ -29,7 +31,7 @@ int main(int argc, char** argv)
     Mesh mesh(vertices, sizeof(vertices) / sizeof(vertices[0]),
                 indices, sizeof(indices) / sizeof(indices[0]) );
 
-    Mesh mesh2("./res/eva.obj");
+    Mesh mesh2("./res/USSEnterprise.obj");
 
     while (!display.IsClosed())
     {
@@ -41,12 +43,13 @@ int main(int argc, char** argv)
         transform.GetPos().x = sinCounter;
         transform.GetPos().z = cosCounter*3;
         transform.GetRot().z = counter;
-        transform.GetRot().y = counter + 180.0;
-        transform.GetRot().x = counter;
+        transform.GetRot().y = -counter + 80.0;
+        transform.GetRot().x = counter+70.0;
 
         shader.Bind();
         shader.Update(transform, camera);
         texture.Bind(0);
+        texture.Bind(1);
         mesh2.Draw();
 
         GLenum myError = glGetError();
--- OpenGLTutorial1/res/basicShader.fsh
+++ OpenGLTutorial1/res/basicShader.fsh
@@ -1,6 +1,8 @@
 #version 120
 
 uniform sampler2D diffuse;
+uniform sampler2D secondTex;
+
 vec3 lightDirection = vec3(0,0,1);
 
 varying vec2 texCoord0;
@@ -8,6 +10,6 @@ varying vec3 normal0;
 
 void main(void)
 {
-	gl_FragColor = texture2D(diffuse, texCoord0)
+	gl_FragColor = texture2D(diffuse, texCoord0) // * texture2D(secondTex, texCoord0)
 		* clamp(dot(-lightDirection, normal0), 0.0, 1.0);
 }
